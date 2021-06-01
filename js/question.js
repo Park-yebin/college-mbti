b35c0f4e2c926773e41e2b0ddaf554edd575fd5c
Park-yebin <81156273+Park-yebin@users.noreply.github.com>
2021-06-01 18:44:43 +0900

    Add status in question pages

--- js/question.js
+++ js/question.js
@@ -1,17 +1,47 @@
 const main = document.querySelector(".main");
 const question = document.querySelector('.question')
+const status = document.querySelector('.status')
+const endPoint = 12;
 
+function addAnswer(answerText, qNum){
+    var a = document.querySelector('.question-answer');
+    var answer = document.createElement('button');
+    answer.classList.add('answerList');
+    answer.classList.add('mx-auto');
+    a.appendChild(answer);
+    answer.innerHTML = answerText;
+    answer.addEventListener("click", function(){
+        var children = document.querySelectorAll(".answerList");
+        for(let i = 0;i < children.length; i++){
+            children[i].disabled = true;
+            children[i].style.display = 'none';
+        }
+        goNext(++qNum);
+    },false);
+}
+function goNext(qNum){
+    var q = document.querySelector('.question-box');
+    q.innerHTML = questionlist[qNum].q;
+    for(let i in questionlist[qNum].a){
+        addAnswer(questionlist[qNum].a[i].answer, qNum);
+    }
+    var status = document.querySelector('.status-bar-matter');
+    status.style.width = (100/endPoint)*(qNum+1)+'%'
+}
 function begin(){
-    main.style.WebkitAnimation = "fadeout 1s";
-    main.style.animation = "fadeout 1s";
+    main.style.WebkitAnimation = "fadeOut 1s";
+    main.style.animation = "fadeOut 1s";
     setTimeout(() => {
-        question.style.WebkitAnimation = "fadein 1s";
-        question.style.animation = "fadein 1s";
+        question.style.WebkitAnimation = "fadeIn 1s";
+        question.style.animation = "fadeIn 1s";
+        status.style.WebkitAnimation = "fadeIn 1s";
+        status.style.animation = "fadeIn 1s";
         setTimeout(() => {
             main.style.display = "none";
             question.style.display = "block";
+            status.style.display = "block";
         }, 450)
+        let qNum = 0;
+        goNext(qNum);
     }, 450);
-    // main.style.display = "none";
-    // question.style.display = "block";
 }
